26738fdd13d8d954c92766924c0dd20f5fe57a5f
halilozat <halilozat@hotmail.com>
2021-07-16 13:35:56 +0300

    Created new react redux counter app

--- react-redux-shopping-cart/src/index.js
+++ react-redux-shopping-cart/src/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/root/App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux'
-import configureStore from './redux/reducers/configureStore'
-import 'alertifyjs/build/css/alertify.min.css'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+import App from './App';
 
-const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 
-reportWebVitals();
